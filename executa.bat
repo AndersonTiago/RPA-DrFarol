2fd22d0c3703612fa90e872653f86ec4406cd7e5
Anderson Tiago <andersonbonamim@gmail.com>
2024-05-29 20:03:46 -0300

    subindo executavel do rpa

---
+++ executa.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Navega até o diretório do projeto
+cd /d D:\Anderson\Projetos\projeto-matheus
+@REM cd /d E:\Documentos\RPA-DrFarol 
+
+REM Executa o comando npm start
+npm start
+
+REM Mantém a janela do prompt de comando aberta após a execução
+pause
